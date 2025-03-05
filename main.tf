4e5130b998fa356f9c746bf72ade47bae2ef6755
João L. Vieira <105939582+jvieira9@users.noreply.github.com>
2025-03-05 04:39:50 -0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,47 +4,47 @@ provider "aws" {
 
 variable "allowed_ip" {
   description = "IP address allowed to access the security group"
-  type        = string
-  default     = "192.0.2.2/32"  # You can change this default IP as needed
+  type = string
+  default = "192.0.2.2/32"  # You can change this default IP as needed
 }
 
 resource "aws_security_group" "tpot_sg" {
-  name        = "tpot_terraform_sg"
+  name = "tpot_terraform_sg"
   description = "Security group for T-Pot EC2 instance"
 
   ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
     cidr_blocks = [var.allowed_ip]
   }
 
   ingress {
-    from_port   = 1
-    to_port     = 64000
-    protocol    = "tcp"
+    from_port = 1
+    to_port = 64000
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 1
-    to_port     = 64000
-    protocol    = "udp"
+    from_port = 1
+    to_port = 64000
+    protocol = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "tpot_terraform" {
-  ami           = "ami-04b4f1a9cf54c11d0"
+  ami = "ami-04b4f1a9cf54c11d0"
   instance_type = "r5.large"
-  key_name      = "vockey"
+  key_name = "vockey"
   security_groups = [aws_security_group.tpot_sg.name]
 
   root_block_device {
@@ -60,7 +60,8 @@ resource "aws_instance" "tpot_terraform" {
       #!/bin/bash
       apt update && apt upgrade -y
       apt install -y git
-      git clone https://github.com/telekom-security/tpotce /home/ubuntu
+      cd /home/ubuntu
+      git clone https://github.com/telekom-security/tpotce
       cd tpotce
       chown -R ubuntu:ubuntu /home/ubuntu/tpotce
       sudo -u ubuntu bash -c "echo -e 'y\nh\njvieira\ny\nPassw0rd\nPassw0rd\ny' | ./install.sh >> /home/ubuntu/tpotce/tf_tpot.log 2>&1"
@@ -71,5 +72,5 @@ resource "aws_instance" "tpot_terraform" {
 
 output "public_ip" {
   description = "Public IP of the EC2 instance"
-  value       = aws_instance.tpot_terraform.public_ip
+  value = aws_instance.tpot_terraform.public_ip
 }
