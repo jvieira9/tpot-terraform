23613c9bdf56bc0461f1ea185fdabb9da7944c7a
João L. Vieira <105939582+jvieira9@users.noreply.github.com>
2025-03-05 03:59:32 -0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,66 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "tpot_sg" {
+  name        = "tpot_terraform_sg"
+  description = "Security group for T-Pot EC2 instance"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["85.241.222.129/32"]
+  }
+
+  ingress {
+    from_port   = 1
+    to_port     = 64000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 1
+    to_port     = 64000
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "tpot_terraform" {
+  ami           = "ami-04b4f1a9cf54c11d0"
+  instance_type = "r5.large"
+  key_name      = "vockey"
+  security_groups = [aws_security_group.tpot_sg.name]
+
+  root_block_device {
+    volume_size = 64  # 64 GB of storage
+    volume_type = "gp2"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "Terraform T-Pot"  # Instance name as a tag
+  }
+  user_data = <<-EOF
+      #!/bin/bash
+      apt update && apt upgrade -y && apt install -y git
+      git clone https://github.com/telekom-security/tpotce /home/ubuntu && cd tpotce
+      chown -R ubuntu:ubuntu /home/ubuntu/tpotce
+      sudo -u ubuntu bash -c "echo -e 'y\nh\njvieira\ny\nPassw0rd\nPassw0rd\ny' | ./install.sh >> /home/ubuntu/tpotce/jvlogs.log 2>&1"
+      sleep 60 && reboot now
+      EOF
+}
+
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.tpot_terraform.public_ip
+}
